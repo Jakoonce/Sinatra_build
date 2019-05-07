4ede65abc853b632d44186857d31cc2afb4c9c9e
Joe Koonce <jakoonce00100@gmail.com>
2019-05-07 17:25:39 -0400

    added the post and delete features

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'bundler'
+Bundler.require
+require './idea'
+
+class IdeaBoxApp < Sinatra::Base
+    set :method_override, true
+    
+    configure :development do
+        register Sinatra::Reloader
+    end
+
+    not_found do
+        erb :error
+    end
+
+    post '/' do
+        # # 1. Create and idea based on the form parameters
+        idea = Idea.new(params['idea_title'], params['idea_description'])
+
+        # # 2. Store it
+        idea.save
+        # # 3. Send us back to the index page to see all ideas
+        redirect '/'
+    end
+
+    get '/' do
+        erb :index, locals: {ideas: Idea.all}
+    end
+
+    delete '/:id' do |id|
+        Idea.delete(id.to_i)
+        redirect '/'
+    end
+
+end
